693c6e283bf4c84d5110457d16113b6d2708218d
albabernal03 <ALBABERO@MYUAX.COM>
2022-10-28 16:12:35 +0200

    cambios

--- ejercicio5.py
+++ ejercicio5.py
@@ -2,17 +2,25 @@ import hashlib
 from sre_parse import WHITESPACE
 #La alianza rebelde necesita comunicarse de manera segura pero el imperio galáctico interviene todas las comunicaciones, por lo que la princesa Leia nos encarga el desarrollo de un algoritmo de encriptación para las comunicaciones rebeldes, que contemple los siguientes requerimientos:
 #cada carácter deberá ser encriptado a ocho caracteres;
+
+
+
 #se deberá generar dos tablas hash para encriptar y desencriptar, para los caracteres desde el “ ” hasta el “}” –es decir desde el 32 al 125 de la tabla ASCII.
 
 
 
+def encriptar():
+    texto = input("Ingrese el texto a encriptar: ")
+    texto_encriptado = ""
+    for i in texto:
+        texto_encriptado += hashlib.sha256(i.encode()).hexdigest()[:8]#[:8] es para que tome los primeros 8 caracteres
 
-def main():
-    mensaje= str(input("Ingrese el mensaje a encriptar: ")).encode('ASCII')
-    sha256= hashlib.sha256(mensaje).hexdigest()
-    with open('mesaje_encriptados.txt', 'w') as file:
-        file.write(sha256)
+    with open("encriptado.txt", "w") as file:
+        file.write(texto_encriptado)
+        print("El texto encriptado se guardo en el archivo encriptado.txt")
 
-if __name__ == '__main__':
-    main()
     
+if __name__ == "__main__":
+    encriptar()
+   
+
