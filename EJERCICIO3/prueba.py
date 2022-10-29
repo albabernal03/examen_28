7941fdd8b1efe99cd6a1c78170b0f2ccfce22764
albabernal03 <ALBABERO@MYUAX.COM>
2022-10-29 14:07:55 +0200

    probando otra de las funciones

--- EJERCICIO3/prueba.py
+++ EJERCICIO3/prueba.py
@@ -3,12 +3,5 @@ from ejercicio3 import *
 lista= crear_naves()
 
 
-
-print("Las naves ordenadas por nombre de forma ascendente son:")
-ordenar__naves_por_nombre_ascedente(lista)
-mostrar_nave(lista)
-
-#ordenar las naves por nombre de forma descendente
-print("Las naves ordenadas por nombre de forma descendente son:")
-ordenar__naves_por_nombre_descendente(lista)
-mostrar_nave(lista)
+print("Las cinco naves con mayor cantidad de tripulacion son:")
+naves_mayor_tripulacion(lista)
