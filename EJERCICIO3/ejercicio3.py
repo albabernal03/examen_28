0c916d129aecd55f2160887bab68cdc619cbeb2b
albabernal03 <ALBABERO@MYUAX.COM>
2022-10-29 14:00:53 +0200

    cambios

--- EJERCICIO3/ejercicio3.py
+++ EJERCICIO3/ejercicio3.py
@@ -48,13 +48,14 @@ def naves_mayor_pasajeros(lista):
 
 
 def naves_mayor_tripulacion(lista):
-    for nave in lista:
-        for nave in range (5):
+    mayor= lista[0]
+    for i in range(5):
+        for nave in lista:
             if nave.tripulacion > mayor.tripulacion:
                 mayor= nave
         print(mayor.nombre, mayor.largo, mayor.tripulacion, mayor.pasajeros)
 
-        
+
 def naves_empiezan_AT(lista):
     for nave in lista:
         if nave.nombre[0] == "A" or nave.nombre[0] == "T":
