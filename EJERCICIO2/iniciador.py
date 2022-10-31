0a2317354e3ad9d634b946e34c4cd6cc55880679
albabernal03 <ALBABERO@MYUAX.COM>
2022-10-31 16:52:50 +0100

    cambios

--- EJERCICIO2/iniciador.py
+++ EJERCICIO2/iniciador.py
@@ -1,15 +1,17 @@
 from ejercicio2 import *
 
 def main():
-    dimension= definir_dimension_matriz()
-    matriz= crear_matriz_vacia(dimension)
-    matriz= cargar_matriz(matriz)
-    mostrar_matriz(matriz)
 
     print('1. Determinante iterativo')
     print('2. Determinante recursivo')
 
     opcion= int(input('Ingrese una opcion: '))
+    
+    dimension= definir_dimension_matriz()
+    matriz= crear_matriz_vacia(dimension)
+    matriz= cargar_matriz(matriz)
+    mostrar_matriz(matriz)
+
     if opcion == 1:
         calcular_determinante_iterativo(matriz)
     elif opcion == 2:
