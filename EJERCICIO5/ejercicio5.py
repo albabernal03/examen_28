63c7d5030a8881522212df6b0d452741bf15cba3
albabernal03 <ALBABERO@MYUAX.COM>
2022-10-31 17:38:54 +0100

    probando donde está el fallo

--- EJERCICIO5/ejercicio5.py
+++ EJERCICIO5/ejercicio5.py
@@ -4,29 +4,37 @@ class TablaHash():
         self.tamanio = tamanio
         self.tabla = [None] * tamanio
 
-    def mostrar_tabla(self):
-        return self.tabla
+    def funcion_hash(self, clave):
+        return clave % self.tamanio
 
-    def funcion_hash(self, caracter):
-        return ord(caracter)*33 % self.tamanio #ord devuelve el codigo ascii del caracter y el 33 es el numero mágico, en los apuntes se definio como funcion de bernstein
-        
-    def agregar(self, elemento):
-        hash= self.funcion_hash(elemento)
-        if self.tabla[hash] == None:
-            self.tabla[hash]= elemento
-        else:
-            print('La posicion esta ocupada')
-
-    def encriptar(self,mensaje):
+    def encriptar(self, mensaje):
         mensaje_encriptado = ''
-        for caracter in mensaje:
-            self.agregar(caracter)
-            resultado = resultado + str(self.funcion_hash(caracter) + 33)
-        return resultado
+        for i in mensaje:
+            mensaje_encriptado += chr(self.funcion_hash(ord(i)))
+        return mensaje_encriptado
 
     def desencriptar(self, mensaje):
         mensaje_desencriptado = ''
-        for caracter in mensaje:
-            mensaje_desencriptado = mensaje_desencriptado + str(self.mensaje_encriptado[ord(caracter)-33])
-
+        for i in mensaje:
+            mensaje_desencriptado += chr(self.funcion_hash(ord(i)))
         return mensaje_desencriptado
+
+
+
+#ejemplo de uso 
+
+mensaje= 'Hola'
+
+Hash= TablaHash(len(mensaje))
+
+mensaje_encriptado= Hash.encriptar(mensaje)
+
+mensaje_desencriptado= Hash.desencriptar(mensaje_encriptado)
+
+print('Mensaje encriptado: ',mensaje_encriptado)
+
+print('Mensaje desencriptado: ',mensaje_desencriptado)
+
+
+
+
