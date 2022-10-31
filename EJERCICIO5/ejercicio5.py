5d8226922b04c6758b57b2fca9ba9d09815fe25a
albabernal03 <ALBABERO@MYUAX.COM>
2022-10-31 11:32:24 +0100

    cambios

--- EJERCICIO5/ejercicio5.py
+++ EJERCICIO5/ejercicio5.py
@@ -9,16 +9,16 @@ from hashlib import sha256
 
 
 
-def encriptar(texto):
+def encriptar(texto, texto_encriptado):
 
-    texto= sha256(texto.encode('utf-8')).hexdigest()[:8]
+    texto_encriptado=  sha256(texto.encode('utf-8')).hexdigest()[:8]
     with open("encriptado.txt", "w") as file:
-        file.write(texto)
+        file.write(texto_encriptado)
         print("El texto encriptado se guardo en el archivo encriptado.txt")
-        print(texto)
+        print(texto_encriptado)
 
-def desencriptar(texto):
-    resolver= texto
+def desencriptar(texto_encriptado):
+    resolver= texto_encriptado
     for x in resolver:
         r= resolver.encode('utf-8')
         a= sha256(r).hexdigest()[:8]
