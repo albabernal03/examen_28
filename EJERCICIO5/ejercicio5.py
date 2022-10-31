0fcd84c542e2776e62befdb43483e744860a3a2a
albabernal03 <ALBABERO@MYUAX.COM>
2022-10-31 17:46:55 +0100

    definimos las funciones

--- EJERCICIO5/ejercicio5.py
+++ EJERCICIO5/ejercicio5.py
@@ -7,5 +7,31 @@ class TablaHash():
     def mostrar_tabla(self):
         '''Muestra la tabla'''
         for i in range(self.tam):
-            print(i, self.tabla[i])
-            
+            print(i, self.tabla[i]) #a través de este print se puede ver como se va llenando la tabla
+
+    def funcion_hash(self,caracter): #utilizamos el número mágico que en los apuntes se define como funcion de bernstein
+        return ord(caracter)*33%self.tam
+
+    def insertar(self,valor):
+        '''Inserta un valor en la tabla'''
+        pos= self.funcion_hash(valor)
+        self.tabla[pos]= valor 
+
+    def encriptar(self,mensaje):
+        '''Encripta un mensaje'''
+        mensaje_encriptado= ''
+        for i in mensaje:
+            self.insertar(i)
+            mensaje_encriptado = mensaje_encriptado + chr(self.funcion_hash(i) + 33) #se le suma 33 para que el caracter sea imprimible
+        return mensaje_encriptado
+
+    def desencriptar(self,mensaje):
+        '''Desencripta un mensaje'''
+        mensaje_desencriptado= ''
+        for i in mensaje:
+            mensaje_desencriptado = mensaje_desencriptado + self.tabla[ord(i)-33]
+        return mensaje_desencriptado
+
+        
+
+
