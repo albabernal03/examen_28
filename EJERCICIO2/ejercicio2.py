e75925d66e9b27f751951792b510bcb9bb086e23
albabernal03 <ALBABERO@MYUAX.COM>
2022-10-31 16:40:00 +0100

    hemos cambiado todo para que pida crear la propia matriz

--- EJERCICIO2/ejercicio2.py
+++ EJERCICIO2/ejercicio2.py
@@ -1,11 +1,44 @@
-#forma recursiva
+
+matriz=[]
+
+def crear_matriz(tamanio):
+    matriz= [None]*tamanio
+    return matriz
+
+def definir_dimension_matriz():
+    dimension= int(input('Ingrese la dimension de la matriz: '))
+    return dimension
+
+def crear_matriz_vacia(dimension):
+    matriz= [None]*dimension
+    for i in range(dimension):
+        matriz[i]= [None]*dimension
+    return matriz
+
+def cargar_matriz(matriz):
+    for i in range(len(matriz)):
+        for j in range(len(matriz)):
+            matriz[i][j]= int(input('Ingrese un numero: '))
+    return matriz
+
+def mostrar_matriz(matriz):
+    for i in range(len(matriz)):
+        print(matriz[i])
+
+def calcular_determinante_iterativo(matriz):
+    print('El determinante de la matriz es: ', determinante_iterativo(matriz))
+
+def calcular_determinante_recursivo(matriz):
+    print('El determinante de la matriz es: ', determinante_recursivo(matriz))
+
+
 def determinante_recursivo(matriz): #forma recursiva ya que se llama a si misma
     if len(matriz) == 1:
         return matriz[0][0]
     else:
         det = 0
         for i in range(len(matriz)):
-            det += (-1)**i * matriz[0][i] * determinante_recursivo([fila[:i] + fila[i+1:] for fila in (matriz[1:])])
+            det += (-1)**i * matriz[0][i] * determinante_recursivo([fila[:i] + fila[i+1:] for fila in (matriz[1:])]) 
         return det
 
 
@@ -19,5 +52,24 @@ def determinante_iterativo(matriz): # es de forma iterativa ya que no se llama a
         for i in range(len(matriz)):
             det += (-1)**i * matriz[0][i] * determinante_iterativo([fila[:i] + fila[i+1:] for fila in (matriz[1:])])
     return det
+
+
+def main():
+    dimension= definir_dimension_matriz()
+    matriz= crear_matriz_vacia(dimension)
+    matriz= cargar_matriz(matriz)
+    mostrar_matriz(matriz)
+    calcular_determinante_iterativo(matriz)
+    calcular_determinante_recursivo(matriz)
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+#forma recursiva
 
 
